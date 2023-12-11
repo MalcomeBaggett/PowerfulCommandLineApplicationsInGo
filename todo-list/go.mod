f47a8c9e6eadded98ae6d905d498a6c2b83794ed
Malcome Baggett <malcomebaggett@gmail.com>
2023-12-11 15:59:52 -0500

    added methods to create a todo list

---
+++ todo-list/go.mod
@@ -0,0 +1,3 @@
+module github.com/MalcomeBaggett/PowerfulCommandLineApplicationsInGo/todo-list
+
+go 1.21.4
---
+++ todo-list/todo.go
@@ -0,0 +1,73 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+)
+
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type List []item
+
+// Add creates a new todo item and appends it to the list
+func (l *List) Add(task string) {
+	t := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+	*l = append(*l, t)
+}
+
+// Complete method marks a todo as as completed
+func (l *List) Completed(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("item %d does not exist", i)
+	}
+	ls[i-1].Done = true
+	ls[i-1].CompletedAt = time.Now()
+
+	return nil
+}
+
+// Delete Method deletes a todo from the list
+func (l *List) Delete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("item %d does not exist", i)
+	}
+	*l = append(ls[:i-1], ls[i:]...)
+	return nil
+}
+
+// Save methods encodes list to JSON and saves it using the provided file name
+func (l *List) Save(filename string) error {
+	js, err := json.Marshal(l)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filename, js, 0644)
+}
+
+// Get method opens the provided file  name, decodes
+// the JSON data and parses it into a list
+func (l *List) Get(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+	return json.Unmarshal(file, l)
+}
